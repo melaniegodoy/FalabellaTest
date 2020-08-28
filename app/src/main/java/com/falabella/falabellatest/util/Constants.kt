6495b844a31adfa15e12da7325773ef37cbf3483
Melanie Godoy <mgodoy@novopayment.com>
2020-08-28 10:48:10 -0400

    saving key securely

--- app/src/main/java/com/falabella/falabellatest/util/Constants.kt
+++ app/src/main/java/com/falabella/falabellatest/util/Constants.kt
@@ -1,7 +1,6 @@
 package com.falabella.falabellatest.util
 
 object Constants {
-    const val KEY = "662ede816988e58fb6d057d9d85605e0"
     const val USERNAME = "user"
     const val PASSWORD = "password"
     const val USER = "Melanie"
